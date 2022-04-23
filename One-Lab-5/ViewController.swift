9c6cd5a91f0d4eaf555761f410b357011d1fb9e4
Farukh Iskalinov <iskalinov133@gmail.com>
2022-04-23 19:48:24 +0600

    Initial Commit

---
+++ One-Lab-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  One-Lab-5
+//
+//  Created by Farukh on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
